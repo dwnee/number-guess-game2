027dd13c49bbe834b59a19b64bae94ba04a04539
Dawon Jung <dwnee0@gmail.com>
2025-02-18 00:19:30 +0900

    second commit

--- main.js
+++ main.js
@@ -13,19 +13,64 @@ let playButton = document.getElementById("play-button")
 playButton.addEventListener("click",play)
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
+let resetButton = document.getElementById("reset-button")
+resetButton.addEventListener("click",reset)
+let rightAnswer = document.getElementById("right-answer")
+let realRemainNumber = 3;
+let remainChance = document.getElementById("remain-chance")
+let gameOver = false;
+let history = []
+
+userInput.addEventListener("focus",function(){
+  userInput.value="";
+})
 
 function pickRandom(){
   computerNum = Math.floor(Math.random()*100) + 1
   console.log("정답",computerNum)
+  rightAnswer.textContent = computerNum;
 }
 function play(){
+  
   let userValue = userInput.value;
+  if(userValue<1 || userValue > 100){
+    resultArea.textContent = "1과 100사이 숫자를 입력해 주세요"
+    return
+  }
+  if(history.includes(userValue)){
+    resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요."
+    return;
+  }
+  realRemainNumber = realRemainNumber-1
   if(userValue < computerNum){
     resultArea.textContent = "UP!!!"
   } else if(userValue > computerNum){
     resultArea.textContent = "DOWN!!!"
   } else {
     resultArea.textContent = "맞췄습니다."
+    gameOver=true;
+  }
+
+  history.push(userValue)
+
+
+  remainChance.textContent = realRemainNumber
+  if(realRemainNumber <=0){
+    gameOver = true;
+  }
+  if(gameOver==true){
+    playButton.disabled = true;
   }
 }
+function reset() {
+  //user input창이 깨끗하게 정리됨
+  userInput.value = ""
+  //새로운 번호 생성
+  pickRandom()
+  resultArea.textContent="결과값이 여기 나옵니다"
+  realRemainNumber = 3;
+  remainChance.textContent = realRemainNumber;
+}
+
+
 pickRandom()
